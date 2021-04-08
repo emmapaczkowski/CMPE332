2d7b7ec3e54e4d194a5f239b43f2b4744926932b
Emma <17ep14@queensu.ca>
2021-04-08 17:09:04 -0400

    display all flight that have the same scheduled aand actuall arrival time closes #4.

--- airline.php
+++ airline.php
@@ -5,6 +5,13 @@
     <title>airline</title>
 </head>
 <body>
+<?php
+include 'connectdb.php';
+?>
     <h1>Welcome to my airline!</h1>
+    <h3> All flights with scheduled arrival times that are the same as actual arrival time</h3>
+    <?php
+    include 'getflights.php';
+    ?>
 </body>
 </html>
---
+++ getflights.php
@@ -0,0 +1,14 @@
+<?php
+$result = $connection->query("SELECT flight_number, actuall_arrival FROM flight WHERE actuall_arrival = scheduled_arrival;");
+echo "      Flight Number  |  Arrival Time";
+echo "<ol>";
+while ($row = $result->fetch()) {
+	echo "<li>";
+	echo $row["flight_number"];
+    echo " | ";
+    echo $row["actuall_arrival"];
+    echo "</li>";
+}
+echo "</ol>";
+?>
+
